159e99d1b9d47ad164f133e7e7ac3bff621827a4
nabetama <mao.nabeta@gmail.com>
2012-09-11 15:38:25 +0900

    create, updateのフックを追加

--- models/bookmark.rb
+++ models/bookmark.rb
@@ -4,5 +4,14 @@ require "sequel"
 DB = Sequel.connect('sqlite://test.db')
 
 class Bookmark < Sequel::Model
+  def before_create
+    self.created_at ||= Time.now
+    super
+  end
+
+  def before_save
+    self.updated_at = Time.now
+    super
+  end
 end
 
